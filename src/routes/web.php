3a62437d6afeea0315db0bc19894a4ab1851136a
iiiyashev <iliya.shevolaev@gmail.com>
2025-07-10 14:21:08 +0300

    fix

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,14 +1,7 @@
 <?php
 
-use App\Models\ApiIntegration\Account;
-use App\Models\TargetApi\Income;
-use App\Services\TargetApiService;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::get('/test', function (TargetApiService $targetApiService) {
-    Income::syncData($targetApiService, Account::first());
 });
